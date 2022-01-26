db27304384a316742e27b7f920edb81e3266f9d9
Kirill Kim <kimdirectkim@yandex.ru>
2022-01-26 16:10:00 +0300

    Добавлена анимация блика тексту

---
+++ js/text.js
@@ -0,0 +1,22 @@
+//GLOWING TEXT НАЧАЛО
+(function($) {
+  var s,
+  spanizeLetters = {
+    settings: {
+      letters: $('.js-spanize'),
+    },
+    init: function() {
+      s = this.settings;
+      this.bindEvents();
+    },
+    bindEvents: function(){
+      s.letters.html(function (i, el) {
+        //spanizeLetters.joinChars();
+        var spanizer = $.trim(el).split("");
+        return '<span>' + spanizer.join('</span><span>') + '</span>';
+      });
+    },
+  };
+  spanizeLetters.init();
+})
+//GLOWING TEXT КОНЕЦ
